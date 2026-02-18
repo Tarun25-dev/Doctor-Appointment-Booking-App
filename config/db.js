5905c6c774f7169d56e4cd742764b54fcb88994a
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-02-18 23:15:00 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(
+      `Mongodb Connected: ${mongoose.connection.host}`.bgGreen.white
+    );
+  } catch (error) {
+    console.log(`Mongodb Server Issue: ${error}`.bgRed.white);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
