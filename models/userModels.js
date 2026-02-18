fea83305a28702c6b633a0c7808b5a43a3e4a9fd
Tharun Kumar <22x51a3225@srecnandyal.edu.in>
2026-02-18 23:18:21 +0530

    Add files via upload

---
+++ models/userModels.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    notifcation: {
+      type: Array,
+      default: [],
+    },
+    seennotification: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
